0cf23785f60574c464496bedd7203b9a8e8759c3
talhariaz105 <muhammad.talha@fabtechsol.com>
2025-07-12 10:18:03 +0500

    previous code

--- api/index.ts
+++
@@ -1,4 +0,0 @@
-import app from '../src/app';
-
-// Export the Express app for Vercel serverless functions
-export default app;
--- src/config/roles.ts
+++ src/config/roles.ts
@@ -1,7 +1,7 @@
 const allRoles = {
   user: ["getSubscriptions","manageCapa"],
   admin: ['getUsers', 'manageCapa', "buySubscription","getSubscriptions","manageUsers"],
-  subAdmin: [""]
+  subAdmin: []
 };
 
 export const roles: string[] = Object.keys(allRoles);
